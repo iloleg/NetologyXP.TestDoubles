d9acf3507f35f66a1b1f48513f35e1a4e0a4ffc6
Oleg <il.oleg@outlook.com>
2017-02-07 11:07:20 +0200

    add new  test

--- tests/when-barmen-pours-drinks.js
+++ tests/when-barmen-pours-drinks.js
@@ -1,15 +1,22 @@
 "use strict";
 
-//var assert = require('chai').assert;
+
+//assert = require('chai').assert;
 var Barmen = require('../src/barmen');
 var Visitor = require('../src/visitor');
 var Cupboard = require('../src/cupboard');
+var CalendarD = require('../dd/calendar_d');
 
 suite('When barmen pours drinks', function () {
     let visitor = {};
     let barmen = {};
+    let cupboard = {};
+    let calendar = {};
+
 
     setup(function () {
+        cupboard = new Cupboard();
+        calendar = new CalendarD();
         visitor = new Visitor();
         visitor.sober();
     });
@@ -21,6 +28,15 @@ suite('When barmen pours drinks', function () {
 
         test('barmen pours x2 volume on a Thursday', function () {
 
+        });
+        test('barmen pours x3 volume on my Birthday', function () {
+            visitor.birthday = new Date(1980, 6, 8);
+            calendar.today = visitor.birthday;
+            let barmen = new Barmen(cupboard);
+
+            let volumeInGlass = barmen.pour('whisky', 100, visitor, calendar);
+
+            assert.equal(3 * 100, volumeInGlass);
         });
 
     });
